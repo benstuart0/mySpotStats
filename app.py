80e0afa3fc9e6778576cad96816183b6ed0cfe21
Ben Stuart <ben@chartmetric.io>
2019-06-23 14:13:27 -0700

    heroku redirect

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ CLIENT_SECRET = CLIENT['secret']
 
 SCOPE = "user-read-private user-top-read playlist-modify-public playlist-modify-private user-read-email playlist-read-private"
 REDIRECT_URI = CLIENT['redirect_uri']
-#REDIRECT_URI = 'http://myspotstats.herokuapp.com/callback' # uncomment for production
+REDIRECT_URI = 'http://myspotstats.herokuapp.com/callback' # uncomment for production
 
 @app.route('/')
 def home():
